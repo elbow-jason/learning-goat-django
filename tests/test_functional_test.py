96c2f81643d039298d80e60165f52ad4b2994b35
jlgoldb2 <jlgoldb2@asu.edu>
2014-12-04 14:49:44 -0700

    tests for multiple items written, but expectedly failing

--- tests/test_functional_test.py
+++ tests/test_functional_test.py
@@ -40,6 +40,7 @@ class NewVisitorTest(unittest.TestCase):
         methodical)
         6. The page updates again, and now shows both items on her list
 
+
         """
 
         #[0]
@@ -58,15 +59,25 @@ class NewVisitorTest(unittest.TestCase):
 
         #[3]
         inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         #[4]
-        inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+
+        self.assertIn(
+            '1: Buy peacock feathers',
+            [row.text for row in rows]
+        )
+
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
         )
 
         #[5]

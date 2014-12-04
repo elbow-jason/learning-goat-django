a06a4cda41dfdff020f4cd02c3875bef43eaab60
jlgoldb2 <jlgoldb2@asu.edu>
2014-12-04 14:54:30 -0700

    refactored for checking of table items

--- tests/test_functional_test.py
+++ tests/test_functional_test.py
@@ -23,6 +23,11 @@ class NewVisitorTest(unittest.TestCase):
         os.environ["webdriver.chrome.driver"] = chromedriver
         self.browser = webdriver.Chrome(executable_path=chromedriver)
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):  #
         """
         Story:
@@ -67,17 +72,12 @@ class NewVisitorTest(unittest.TestCase):
 
         #[4]
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-
-        self.assertIn(
-            '1: Buy peacock feathers',
-            [row.text for row in rows]
+        self.check_for_row_in_list_table(
+            '1: Buy peacock feathers'
         )
 
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly'
         )
 
         #[5]

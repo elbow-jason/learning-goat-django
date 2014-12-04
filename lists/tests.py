7087326d95daa1eaa3a2b247b54c9de8916b8e0d
jlgoldb2 <jlgoldb2@asu.edu>
2014-12-04 15:06:30 -0700

    unit tests for save/retrieve item

--- lists/tests.py
+++ lists/tests.py
@@ -1,10 +1,35 @@
 from django.test import TestCase
 from django.core.urlresolvers import resolve
-from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 # Create your tests here.
+from lists.views import home_page
+from lists.models import Item
+
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        text1 = 'The first (ever) list item'
+        text2 = 'Item the second'
+
+        first_item = Item()
+        first_item.text = text1
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = text2
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+
+        self.assertEqual(first_saved_item.text, text1)
+        self.assertEqual(second_saved_item.text, text2)
 
 
 class HomePageTest(TestCase):

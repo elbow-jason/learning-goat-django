0c2df76b0abf5d37447888621b8c1449cdf2dc55
jlgoldb2 <jlgoldb2@asu.edu>
2014-12-05 14:53:35 -0700

    FT uses unittest instance's assertRegexpMatches vice assertRegex

--- functional_tests/test_new_visitor.py
+++ functional_tests/test_new_visitor.py
@@ -34,44 +34,34 @@ class NewVisitorTest(LiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):  #
         """
         Story:
-        0. Edith has heard about a cool new online to-do app. She goes
-        to check out its homepage
 
-        1. She notices the page title and header mention to-do lists
-        2. She is invited to enter a to-do item straight away
-        3. She types "Buy peacock feathers" into a text box (Edith's hobby
-        is tying fly-fishing lures)
-        4. When she hits enter, the page updates, and now the page lists
-        "1: Buy peacock feathers" as an item in a to-do list table
         5. There is still a text box inviting her to add another item. She
         enters "Use peacock feathers to make a fly" (Edith is very
         methodical)
-        6. The page updates again, and now shows both items on her list
-
-
         """
 
-        #[0]
-        #self.browser.get('http://localhost:8000')
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
         self.browser.get(self.live_server_url)
 
-        #[1]
+        # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        #[2]
+        # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(inputbox.get_attribute('placeholder'),
                          'Enter a to-do item'
                          )
 
-        #[3]
+        # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
+
         # When she hits enter, she is taken to a new URL,
         # and now the page lists "1: Buy peacock feathers" as an item in a
         # to-do list table
-
         inputbox.send_keys(Keys.ENTER)
         edith_list_url = self.browser.current_url
         self.assertRegexpMatches(edith_list_url, '/lists/.+')
@@ -79,17 +69,42 @@ class NewVisitorTest(LiveServerTestCase):
         #inputbox = self.browser.find_element_by_id('id_new_item')
         #inputbox.send_keys('Use peacock feathers to make a fly')
         #inputbox.send_keys(Keys.ENTER)
-
-        #[4]
         self.check_for_row_in_list_table(
             '1: Buy peacock feathers'
         )
-
         self.check_for_row_in_list_table(
             '2: Use peacock feathers to make a fly'
         )
 
-        #[5]
+        # Now a new user, Francis, comes along to the site.
+
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies etc..
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        francis_list_url = self.browser.current_url
+        self.assertRegexpMatches(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+
+        # then everyone, having listed their individual
+        # to-do items.. goes to bed.
         self.fail("Finish the test!")  #
 
 if __name__ == '__main__':

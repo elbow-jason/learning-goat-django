9fcba4efd6ad001051702c473b36f0f532445e0b
jlgoldb2 <jlgoldb2@asu.edu>
2014-12-05 16:07:27 -0700

    re-added  urls.py in superlists

---
+++ superlists/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/the-only-list-in-the-world/$', 'lists.views.view_list',
+        name='view_list'
+    ),
+    # Examples:
+    # url(r'^blog/', include('blog.urls')),
+    # url(r'^admin/', include(admin.site.urls)),
+)
